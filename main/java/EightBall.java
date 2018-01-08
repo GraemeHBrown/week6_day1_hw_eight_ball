753267508c2cb86e1e1858d9547552f9014c9917
Graeme Brown <graemehbrown@googlemail.com>
2018-01-08 19:41:47 +0000

    Added @Before test setup to test class for EightBall also test to show size of numbers list being passed in to constructor.
    Added constructor to EightBall class and count method to return size of numbers list.

--- main/java/EightBall.java
+++ main/java/EightBall.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class EightBall {
+    private ArrayList<Integer> numbers;
+
+    public EightBall(ArrayList<Integer> numbersToAdd){
+        this.numbers = numbersToAdd;
+    }
+
+    public int getNumberCount() {
+        return this.numbers.size();
+    }
 }
--- test/java/EightBallTest.java
+++ test/java/EightBallTest.java
@@ -1,11 +1,22 @@
 import org.junit.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class EightBallTest {
 
     EightBall eightball;
 
     @Before
     public void before(){
-        eightball = new EightBall();
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1,4,8));
+        eightball = new EightBall(numbers);
+    }
+
+    @Test
+    public void eightBallHasThreeStartingNumbers(){
+        assertEquals(3, eightball.getNumberCount());
     }
 }
